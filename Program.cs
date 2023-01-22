ed1ed871b30096a1ba2adf78fcb31c4049cc0cfe
Gustavo M <gmatos05@gmail.com>
2023-01-22 17:53:31 -0300

    Exercicio_reflection_c#

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿ using System;
+ using System.Reflection;
+namespace Exercicio_Reflection{
+    class Program{
+        public static void Main(){
+            
+        DisplayPublicProperties();
+        CreateInstance();
+        }
+        public static void DisplayPublicProperties(){
+            Student student =  new Student();
+            PropertyInfo[] propertyInfos = student.GetType().GetProperties();
+            foreach (var propertyInfo in propertyInfos){
+                System.Console.WriteLine($"{propertyInfo.Name}");
+            }
+        }
+        public static void CreateInstance(){
+            Type t = typeof(Student);
+            object objeto = Activator.CreateInstance(t);
+
+            PropertyInfo propriedade_Name = t.GetProperty("Name");
+            propriedade_Name.SetValue(objeto,"Nome");
+
+            PropertyInfo propriedade_rollNumber = t.GetProperty("RollNumber");
+            propriedade_rollNumber.SetValue(objeto,45486);
+
+            PropertyInfo propriedade_faculdade = t.GetProperty("University");
+            propriedade_faculdade.SetValue(objeto,"Nome da Faculdade");
+
+            MethodInfo displayMethod =t.GetMethod("DisplayInfo");
+            displayMethod.Invoke(objeto,null);
+        }
+    }
+   
+        
+}
---
+++ Student.cs
@@ -0,0 +1,13 @@
+namespace Exercicio_Reflection{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int RollNumber { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} - {University} - {RollNumber}");
+        }
+    }
+}
